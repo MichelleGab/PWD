9fd6c500e763b8b5f21cb71ce5ad821cfb15f4aa
MichelleGab <240712798@students.uajy.ac.id>
2025-11-14 00:12:45 +0700

    Add files via upload

---
+++ activity-mandiri/js/script.js
@@ -0,0 +1,30 @@
+function fn_ValForm() {
+	var sMsg = "";
+
+	var name    = document.getElementById("name").value;
+	var email   = document.getElementById("email").value;
+	var message = document.getElementById("message").value;
+
+	var emailPattern = /^[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9]@[a-z0-9][a-z0-9_\.-]{0,}[a-z0-9][\.][a-z0-9]{2,4}$/;
+
+	if (name == "") {
+		sMsg += "\n* Anda belum mengisikan nama";
+	}
+
+	if (email == "") {
+		sMsg += "\n* Anda belum mengisikan email";
+	} else if (!emailPattern.test(email)) {
+		sMsg += "\n* Email yang Anda masukkan tidak valid";
+	}
+
+	if (message == "") {
+		sMsg += "\n* Anda belum mengisikan pesan";
+	}
+
+	if (sMsg != "") {
+		alert("Peringatan:\n" + sMsg);
+		return false;  
+	} else {
+		return true;   
+	}
+}
